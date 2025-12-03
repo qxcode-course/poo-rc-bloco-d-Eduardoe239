61322cfdd1948ded30f65113d03d1b6775358d90
Luís Eduardo <148898517+Eduardoe239@users.noreply.github.com>
2025-12-03 22:23:11 +0000

    mlk python corrompeu perdi umas 2 hrs e meia no tko e perdi boa parte do progreço do code mais ai deu bom pq ficou salvo no vs code local e eu transferi pro codespace

--- tasks/poo/agenda/py/draft.py
+++ tasks/poo/agenda/py/draft.py
@@ -4,11 +4,7 @@ class Fone:
         self._number = number
 
     def isValid(self):
-        valid_chars = "0123456789"
-        for c in self._number:
-            if c not in valid_chars:
-                return False
-        return True
+        return all(c.isdigit() for c in self._number)
 
     def getId(self):
         return self._id
@@ -36,9 +32,149 @@ class Contact:
     def rmFone(self, index):
         if index < 0 or index >= len(self._fones):
             print("fail: invalid index")
+            return
+        self._fones.pop(index)
+
+    def toggleFavorited(self):
+        self._favorited = not self._favorited
+
+    def isFavorited(self):
+        return self._favorited
+
+    def getFones(self):
+        return self._fones
+
+    def getName(self):
+        return self._name
+
+    def __str__(self):
+        prefix = "@ " if self._favorited else "- "
+        fones_str = ", ".join(str(f) for f in self._fones)
+        return f"{prefix}{self._name} [{fones_str}]"
+
+
+class Agenda:
+    def __init__(self):
+        self._contacts = []
+
+    def findPosByName(self, name):
+        for i, c in enumerate(self._contacts):
+            if c.getName() == name:
+                return i
+        return -1
+
+    def addContact(self, name, fones):
+        pos = self.findPosByName(name)
+        if pos != -1:
+            cont = self._contacts[pos]
+            for id_, num in fones:
+                cont.addFone(id_, num)
         else:
-            new_fones = []
-            for i in range(len(self._fones)):
-                if i != index:
-                    new_fones.append(self._fones[i])
-            self._fones = new_fones
+            cont = Contact(name)
+            for id_, num in fones:
+                cont.addFone(id_, num)
+            self._contacts.append(cont)
+            self._contacts.sort(key=lambda c: c.getName())
+
+    def getContact(self, name):
+        pos = self.findPosByName(name)
+        if pos == -1:
+            return None
+        return self._contacts[pos]
+
+    def rmContact(self, name):
+        pos = self.findPosByName(name)
+        if pos != -1:
+            self._contacts.pop(pos)
+
+    def search(self, pattern):
+        pat = pattern.lower()
+        return [c for c in self._contacts if pat in str(c).lower()]
+
+    def getFavorited(self):
+        return [c for c in self._contacts if c.isFavorited()]
+
+    def __str__(self):
+        return "\n".join(str(c) for c in self._contacts)
+
+
+def parse_fones(tokens):
+    fones = []
+    for t in tokens:
+        if ":" in t:
+            id_, num = t.split(":", 1)
+            fones.append((id_, num))
+    return fones
+
+
+def main():
+    agenda = Agenda()
+    while True:
+        try:
+            line = input()
+        except EOFError:
+            break
+
+        if not line:
+            continue
+
+        line = line.strip()
+        if line == "":
+            continue
+
+        # imprime comando com $
+        print(f"${line}")
+
+        parts = line.split()
+
+        if parts[0] == "add":
+            name = parts[1]
+            fones = parse_fones(parts[2:])
+            agenda.addContact(name, fones)
+
+        elif parts[0] == "show":
+            print(agenda)
+
+        elif parts[0] == "rmFone":
+            if len(parts) != 3:
+                print("fail: invalid command")
+                continue
+            name = parts[1]
+            try:
+                idx = int(parts[2])
+            except:
+                print("fail: invalid index")
+                continue
+
+            c = agenda.getContact(name)
+            if c is None:
+                print("fail: contact not found")
+            else:
+                c.rmFone(idx)
+
+        elif parts[0] == "rm":
+            if len(parts) == 2:
+                agenda.rmContact(parts[1])
+
+        elif parts[0] == "search":
+            pad = line.split(" ", 1)[1]
+            results = agenda.search(pad)
+            for r in results:
+                print(r)
+
+        elif parts[0] == "tfav":
+            name = parts[1]
+            c = agenda.getContact(name)
+            if c is None:
+                print("fail: contact not found")
+            else:
+                c.toggleFavorited()
+
+        elif parts[0] == "favs":
+            for c in agenda.getFavorited():
+                print(c)
+
+        elif parts[0] == "end":
+            break
+
+main()
